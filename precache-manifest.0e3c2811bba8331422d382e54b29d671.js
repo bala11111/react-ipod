fd59654421933c61589ba2f8b8edccae36e3f801
bala11111 <balki.98@gmail.com>
2020-10-06 16:00:15 +0530

    Updates

---
+++ precache-manifest.0e3c2811bba8331422d382e54b29d671.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329528ebe2f5a365c1f1a744e86ed93b",
+    "url": "/react-ipod/index.html"
+  },
+  {
+    "revision": "5b40b5c8d2d1bbf3da10",
+    "url": "/react-ipod/static/css/main.f50f0f23.chunk.css"
+  },
+  {
+    "revision": "3d8c5872dde98f93dcd2",
+    "url": "/react-ipod/static/js/2.400625ad.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/react-ipod/static/js/2.400625ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b40b5c8d2d1bbf3da10",
+    "url": "/react-ipod/static/js/main.bd0be2bd.chunk.js"
+  },
+  {
+    "revision": "1af1cd818c6a6790991c",
+    "url": "/react-ipod/static/js/runtime-main.b9ab00ad.js"
+  },
+  {
+    "revision": "d7770aa6e9c7d80748d401dec3d810c6",
+    "url": "/react-ipod/static/media/games.d7770aa6.png"
+  },
+  {
+    "revision": "a234ddf322877cfcf8bd8f3bf6785942",
+    "url": "/react-ipod/static/media/img1.a234ddf3.jpg"
+  },
+  {
+    "revision": "216ca72e11da76a4c58f34cd0ccdfe78",
+    "url": "/react-ipod/static/media/img2.216ca72e.jpeg"
+  },
+  {
+    "revision": "0c79dd3a5b80ce30355253ef6c712441",
+    "url": "/react-ipod/static/media/img3.0c79dd3a.jpg"
+  },
+  {
+    "revision": "1d4751a52c33a786a858c7cda5c31f42",
+    "url": "/react-ipod/static/media/kutti.1d4751a5.mp3"
+  },
+  {
+    "revision": "8310ec95231ef30b465628880ff75e39",
+    "url": "/react-ipod/static/media/settings.8310ec95.png"
+  },
+  {
+    "revision": "3f3404bc6055736ee2c8edff70e1b877",
+    "url": "/react-ipod/static/media/vc.3f3404bc.mp3"
+  },
+  {
+    "revision": "8d8ff49e81a0c42a58a7268afef50374",
+    "url": "/react-ipod/static/media/vr.8d8ff49e.mp3"
+  }
+]);
